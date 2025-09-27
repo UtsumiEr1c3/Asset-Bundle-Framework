7ea0acbdbfc647b806a685e9a80e2e0443029546
UtsumiEr1c3 <glhf2233@163.com>
2025-09-27 19:54:00 +0800

    add: switch platform and load setting

---
+++ Assets/AssetBundleFramework/Editor/BuildSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class BuildSetting : ISupportInitialize
+{
+    public void BeginInit()
+    {
+
+    }
+
+    public void EndInit()
+    {
+
+    }
+}
--- Assets/AssetBundleFramework/Editor/Builder.cs
+++ Assets/AssetBundleFramework/Editor/Builder.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEditor;
 using UnityEngine;
 
 public static class Builder
 {
+    private static readonly Profiler ms_BuildProfiler = new Profiler(nameof(Builder));
+    private static readonly Profiler ms_LoadBuildSettingProfiler = ms_BuildProfiler.CreateChild(nameof(LoadSetting));
+    private static readonly Profiler ms_SwitchPlayformProfiler = ms_BuildProfiler.CreateChild(nameof(SwitchPlatform));
+
+#if UNITY_IOS
+    private const string PLATFORM = "iOS";
+#elif UNITY_ANDROID
+    private const string PLATFORM = "Android";
+#else
+    private const string PLATFORM = "Windows";
+#endif
+
+    // 打包设置
+    public static BuildSetting buildSetting { get; private set; }
+
     #region Build MenuItem
 
     [MenuItem("Tools/ResBuild/Windows")]
     public static void BuildWindows()
     {
         Debug.Log("aaa");
+    }
+
+    public static void SwitchPlatform()
+    {
+        string platform = PLATFORM;
+
+        switch (platform)
+        {
+            case "Windows":
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64);
+                break;
+            case "Android":
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+                break;
+            case "iOS":
+                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.iOS);
+                break;
+        }
+    }
+
+    private static BuildSetting LoadSetting(string settingPath)
+    {
+        //buildSetting = 
+
+        return buildSetting;
     }
     
 
