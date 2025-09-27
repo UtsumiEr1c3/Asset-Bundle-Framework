fce09335e53b06a5881bfa8af56aea708d0f80d0
UtsumiEr1c3 <glhf2233@163.com>
2025-09-27 13:10:16 +0800

    Init

---
+++ Assets/AssetBundleFramework/Editor/Builder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class Builder
+{
+    #region Build MenuItem
+
+    [MenuItem("Tools/ResBuild/Windows")]
+    public static void BuildWindows()
+    {
+        Debug.Log("aaa");
+    }
+    
+
+    #endregion
+}
