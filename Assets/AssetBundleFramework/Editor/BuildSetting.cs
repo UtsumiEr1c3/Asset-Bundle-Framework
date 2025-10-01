ee9dfebb0657396a43d86b90cee2adb7ec3ab4bd
UtsumiEr1c3 <glhf2233@163.com>
2025-10-02 01:18:00 +0800

    add: bundle file analyze and collect function

--- Assets/AssetBundleFramework/Editor/BuildSetting.cs
+++ Assets/AssetBundleFramework/Editor/BuildSetting.cs
@@ -3,18 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 using log4net.Filter;
+using UnityEditor;
 using UnityEngine;
 
 public class BuildSetting : ISupportInitialize
 {
+    [DisplayName("项目名称")]
+    [XmlAttribute("ProjectName")]
+    public string projectName { get; set; }
+
+    [DisplayName("后缀列表")]
+    [XmlAttribute("SuffixList")]
+    public List<string> suffixList { get; set; }
+
     [DisplayName("打包文件的目录文件夹")]
     [XmlAttribute("BuildRoot")]
     public string buildRoot { get; set; }
 
     [DisplayName("打包选项")]
-    [XmlAttribute("BuildItem")]
+    [XmlElement("BuildItem")]
     public List<BuildItem> items{ get; set; }
 
     [XmlIgnore]
@@ -60,5 +70,85 @@ public class BuildSetting : ISupportInitialize
             }
             itemDic.Add(buildItem.assetPath, buildItem);
         }
+    }
+
+    /// <summary>
+    /// 获取所有在打包设置的文件列表
+    /// </summary>
+    /// <returns></returns>
+    public HashSet<string> Collect()
+    {
+        float min = Builder.collectTuleFileProgress.x;
+        float max = Builder.collectTuleFileProgress.y;
+
+        EditorUtility.DisplayProgressBar($"{nameof(Collect)}", "搜集打包规则资源", min);
+
+        // 处理每个规则忽略的目录, 比如路径A/B/C. 需要忽略A/B
+        for (int i = 0; i < items.Count; i++)
+        {
+            BuildItem buildItem_i = items[i];
+
+            if (buildItem_i.resourceType != EResourceType.Direct)
+            {
+                continue;
+            }
+            buildItem_i.ignorePaths.Clear();
+            for (int j = 0; j < items.Count; j++)
+            {
+                BuildItem buildItem_j = items[j];
+                if (i != j && buildItem_j.resourceType == EResourceType.Direct)
+                {
+                    if (buildItem_j.assetPath.StartsWith(buildItem_i.assetPath, StringComparison.InvariantCulture))
+                    {
+                        buildItem_i.ignorePaths.Add(buildItem_j.assetPath);
+                    }
+                }
+            }
+        }
+
+        // 存储被规则分析到的所有文件
+        HashSet<string> files = new HashSet<string>();
+        for (int i = 0; i < items.Count; i++)
+        {
+            BuildItem buildItem = items[i];
+
+            EditorUtility.DisplayProgressBar($"{nameof(Collect)}", "搜集打包规则资源", min + (max - min) * ((float)i / items.Count - 1));
+
+            if (buildItem.resourceType != EResourceType.Direct)
+            {
+                continue;
+            }
+
+            List<string> tempFiles = Builder.GetFiles(buildItem.assetPath, null, buildItem.suffixes.ToArray());
+            for (int j = 0; j < tempFiles.Count; j++)
+            {
+                string file = tempFiles[j];
+
+                // 过滤被忽略的
+                if (IsIgnore(buildItem.ignorePaths, file))
+                {
+                    continue;
+                }
+
+                files.Add(file);
+            }
+
+            EditorUtility.DisplayProgressBar($"{nameof(Collect)}", "搜集打包设置资源", (float)(i + 1) / items.Count);
+        }
+
+        return files;
+    }
+
+    /// <summary>
+    /// 文件是否在忽略列表里
+    /// </summary>
+    /// <param name="ignoreList"></param>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    public bool IsIgnore(List<string> ignoreList, string file)
+    {
+        // TODO:
+
+        return false;
     }
 }
--- Assets/AssetBundleFramework/Editor/Builder.cs
+++ Assets/AssetBundleFramework/Editor/Builder.cs
@@ -10,9 +10,13 @@ using UnityEngine;
 
 public static class Builder
 {
+    public static readonly Vector2 collectTuleFileProgress = new Vector2(0, 0.2f);
+
     private static readonly Profiler ms_BuildProfiler = new Profiler(nameof(Builder));
     private static readonly Profiler ms_LoadBuildSettingProfiler = ms_BuildProfiler.CreateChild(nameof(LoadSetting));
     private static readonly Profiler ms_SwitchPlayformProfiler = ms_BuildProfiler.CreateChild(nameof(SwitchPlatform));
+    private static readonly Profiler ms_CollectProfiler = ms_BuildProfiler.CreateChild(nameof(Collect));
+    private static readonly Profiler ms_CollectBuildSettingFileProfiler = ms_CollectProfiler.CreateChild("CollectBuildSettingFile");
 
 #if UNITY_IOS
     private const string PLATFORM = "iOS";
@@ -26,12 +30,15 @@ public static class Builder
 
     public static string buildPath { get; private set; } // 打包目录
 
+    public static readonly string BuildSettingPath = Path.GetFullPath("BuildSetting.xml").Replace("\\", "/"); // 打包配置
+
     #region Build MenuItem
 
     [MenuItem("Tools/ResBuild/Windows")]
     public static void BuildWindows()
     {
         Debug.Log("执行了BuildWindows");
+        Build();
     }
 
     public static void SwitchPlatform()
@@ -72,7 +79,56 @@ public static class Builder
 
         return buildSetting;
     }
-    
+
+    private static void Build()
+    {
+        ms_BuildProfiler.Start();
+
+        ms_SwitchPlayformProfiler.Start();
+        SwitchPlatform();
+        ms_SwitchPlayformProfiler.Stop();
+
+        ms_LoadBuildSettingProfiler.Start();
+        buildSetting = LoadSetting(BuildSettingPath);
+        ms_LoadBuildSettingProfiler.Stop();
+
+        // 搜集Bundle信息
+        ms_CollectProfiler.Start();
+        Dictionary<string, List<string>> bundleDic = Collect();
+        ms_CollectProfiler.Stop();
+
+        ms_BuildProfiler.Stop();
+
+        Debug.Log($"打包完成{ms_BuildProfiler}");
+    }
+
+    /// <summary>
+    /// 搜集打包bundle的信息
+    /// </summary>
+    /// <returns></returns>
+    private static Dictionary<string, List<string>> Collect()
+    {
+        // 获取所有打包设置的文件列表
+        ms_CollectBuildSettingFileProfiler.Start();
+        HashSet<string> files = buildSetting.Collect();
+        ms_CollectBuildSettingFileProfiler.Stop();
+    }
+
+    /// <summary>
+    /// 获取指定路径的文件
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="prefix"></param>
+    /// <param name="suffixes"></param>
+    /// <returns></returns>
+    public static List<string> GetFiles(string path, string prefix, params string[] suffixes)
+    {
+        List<string> result = new List<string>();
+
+        // TODO:
+
+        return result;
+    }
 
     #endregion
 }
